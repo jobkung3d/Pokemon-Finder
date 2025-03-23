4b0c890a92c098f48722977025e63f13d3527ec0
jobkung3d <worachat0137@gmail.com>
2025-03-23 23:38:19 +0700

    update fetch data

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
-import Header from './components/Header'
+import Header from './components/Header';
+import PokeData from "./components/PokeData";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <PokeData />
     </>
   );
 }
